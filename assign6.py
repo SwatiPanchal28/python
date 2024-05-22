84345e23722b41878f4a7f22d0030237e312c156
swati panchal <115784584+SwatiPanchal28@users.noreply.github.com>
2024-05-22 18:14:06 +0530

    Add files via upload

---
+++ assign6.py
@@ -0,0 +1,21 @@
+name="shubhangi"
+age=25
+print("name :",name,"age :",age)
+
+name="john"
+var=232
+s=21.233
+print("name :",name)
+print(type(name))
+print(type(var))
+print(type(s))
+
+a=10
+b=23.5
+print(type(a))
+print(type(b))
+
+a=50
+b=a
+print(id(a))
+print(id(b))
